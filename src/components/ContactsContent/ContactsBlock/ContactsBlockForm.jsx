b8df323b3805141c83c2e2eec0ef9c650ccc0023
kykakre <kykakre@mail.ru>
2022-10-08 06:12:29 +0400

    new pages

---
+++ src/components/ContactsContent/ContactsBlock/ContactsBlockForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConatctsBlockForm = (props) => {
+    return (
+        <div className={style.}>
+
+        </div>
+    );
+};
+
+export default ConatctsBlockForm;
