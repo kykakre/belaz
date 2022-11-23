1579be9f45cde10448d30e31f94447b65f6f3fae
kykakre <kykakre@mail.ru>
2022-11-23 15:18:22 +0400

    fix pravki

--- src/components/ContactsContent/ContactsBlock/ContactsBlockInfo.jsx
+++ src/components/ContactsContent/ContactsBlock/ContactsBlockInfo.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 import ContactsBlockInfoItem from "./ContactsBlockInfoItem.jsx";
-import style from "./ContactsBlock.module.scss"
+import style from "./ContactsBlock.module.scss";
 const ContactsBlockInfo = (props) => {
-    let item = props.contacts.map((e)=>(
-        <ContactsBlockInfoItem id={e.id} key={e.id} icon={e.icon} title={e.title} text={e.text}/>
-    ))
-    return (
-        <div className={style.block}>
-            {item}
-        </div>
-    );
+  let item = props.contacts.map((e) => (
+    <ContactsBlockInfoItem
+      id={e.id}
+      key={e.id}
+      icon={e.icon}
+      title={e.title}
+      text={e.text}
+    />
+  ));
+  return <div className={style.block}>{item}</div>;
 };
 
 export default ContactsBlockInfo;
--- src/components/ContactsMini/ContactsMin.jsx
+++ src/components/ContactsMini/ContactsMin.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import style from "./ContactsMini.module.scss"
+import React from "react";
+import style from "./ContactsMini.module.scss";
 const ContactsMin = (props) => {
-    return (
-        <div className={style.block}>
-            <a href="tel:+73432875732" className={style.link}>+7 (343) 287-57-32</a>
-            <a href="mailto:info@agregatekb.ru" className={style.link}>info@agregatekb.ru</a>
-            <div className={style.text}>Интернет-магазин запчастей
-                для легковых автомобилей, грузовиков и спецтехники</div>
-        </div>
-    );
+  return (
+    <div className={style.block}>
+      <a href="tel:+73432875732" className={style.link}>
+        +7 (343) 287-57-32
+      </a>
+      <a href="tel:+79655327026" className={style.link}>
+        +7 (965) 532-70-26
+      </a>
+      <a href="tel:+79222095332" className={style.link}>
+        +7 (922) 209-53-32
+      </a>
+      <a href="mailto:info@agregatekb.ru" className={style.link}>
+        info@agregatekb.ru
+      </a>
+      <div className={style.text}>
+        Интернет-магазин запчастей для легковых автомобилей, грузовиков и
+        спецтехники
+      </div>
+    </div>
+  );
 };
 
 export default ContactsMin;
