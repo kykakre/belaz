c2311c9d1e47e5b62558153415c140cf7576d341
kykakre <kykakre@mail.ru>
2022-11-25 12:20:42 +0400

    footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = (props) => {
         <div className={style.item}>
           <img src={logo} className={style.logo} />
           <div className={style.text}>
-            620012, город Екатеринбург, улица Посадская, 16а, корпус И,
+            620102, город Екатеринбург, улица Посадская, 16а, корпус И,
             помещение И-101
           </div>
           <div className={style.text}>ПН — ПТ: с 9:00 до 17:00</div>
@@ -76,8 +76,14 @@ const Footer = (props) => {
         />
         <div className={style.itemLast}>
           <div className={style.linkPhone}>Заказать звонок</div>
-          <a href="tel:+73432875732" className={style.phone}>
+          <a href="https://wa.me/73432875732" className={style.phone}>
             +7 (343) 287-57-32
+          </a>
+          <a href="https://wa.me/79655327026" className={style.phone}>
+            +7 (965) 532-70-26
+          </a>
+          <a href="https://wa.me/79222095332" className={style.phone}>
+            +7 (922) 209-53-32
           </a>
           <a href="mailto:info@agregatekb.ru" className={style.text}>
             info@agregatekb.ru
